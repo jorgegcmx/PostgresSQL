07386da0f69dd6912be42474678fc66fe8a4134d
Jorge Antonio Gonzalez Camacho <50755574+jorgegcmx@users.noreply.github.com>
2023-09-22 18:40:05 -0600

    Update http extension.sql

--- http extension.sql
+++ http extension.sql
@@ -1,1 +1,8 @@
-
+SELECT content::json->'data' as respuestaWS-->>'body' 
+  FROM http((
+           'POST',
+           'url',
+            ARRAY[http_header('Authorization','Basic ')],
+           'application/json',
+           '{"params":"value"}'
+        )::http_request);
